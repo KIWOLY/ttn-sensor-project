f2bfa3bd114b8bacb0c888fc0ed6c3e89bf96da3
Kiwoly <ikiwoly@gmail.com>
2025-05-07 21:58:04 +0300

    Add SensorData model and admin registration

--- sensor_app/models.py
+++ sensor_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class SensorData(models.Model):
+    alcohol = models.FloatField()
+    benzene = models.FloatField()
+    co2 = models.FloatField()
+    coord_lat = models.FloatField()
+    coord_lon = models.FloatField()
+    humidity = models.FloatField()
+    nox = models.FloatField()
+    pressure = models.FloatField()
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SensorData at {self.created_at}"
